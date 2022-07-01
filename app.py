62ae698d28133fca5d0b841eeefabd7de190afd4
Celia Andrade <celiasra@uol.com.br>
2022-06-30 21:22:48 -0300

    ajuste app

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ class Dados(Resource):
 api.add_resource(Dados, '/dados_aluno/<int:ra>')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
